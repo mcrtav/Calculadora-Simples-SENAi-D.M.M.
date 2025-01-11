9fa4384e0b84f34ee6a4b2aee4ec561c1658a822
Miriam Cristina <58282536+mcrtav@users.noreply.github.com>
2025-01-11 19:31:19 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -13,3 +13,40 @@ function sub() {
     console.log(result);
     document.querySelector("#result").value = result
 }
+
+
+function sub() {
+    var result =
+        Number(document.getElementById("firstNo").value) -
+        Number(document.getElementById("secondNo").value);
+    console.log(result);
+    document.querySelector("#result").value = result
+}
+
+
+
+function mult() {
+    var result =
+        Number(document.getElementById("firstNo").value) *
+        Number(document.getElementById("secondNo").value);
+    console.log(result);
+    document.querySelector("#result").value = result
+}
+
+
+function div() {
+
+
+    var result
+       var n1= Number(document.getElementById("firstNo").value) ;
+       var n2 = Number(document.getElementById("secondNo").value);
+       result= n1/n2
+    if (secondNo === 0){
+        document.querySelector("#result").value = "Error div 0"
+
+    }else{
+        document.querySelector("#result").value = result
+    }
+        console.log(result);
+   
+}
